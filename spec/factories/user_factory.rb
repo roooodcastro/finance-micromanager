13d25dd02256323536c94cb5d0610936d62f5ea6
Rodrigo Azevedo <rod.c.azevedo@gmail.com>
2023-11-08 11:23:50 +0000

    Fix specs

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -7,6 +7,12 @@ FactoryBot.define do
     sequence(:email) { |n| "email_#{n}@email.com" }
     sequence(:password) { |n| "testing#{n}" }
 
+    default_account { build(:account, user: nil) }
+
+    after(:build) do |user|
+      user.default_account.user = user
+    end
+
     after(:create, &:confirm)
   end
 end
