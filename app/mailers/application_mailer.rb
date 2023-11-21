ee0bee59912735aeee1f6e1c0436825de19bcd99
Rodrigo Azevedo <rod.c.azevedo@gmail.com>
2023-11-21 11:05:49 +0000

    Fix default mail from address

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: Rails.application.credentials.default_email_from
+
   layout 'mailer'
 end
