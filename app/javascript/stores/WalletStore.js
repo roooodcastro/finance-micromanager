b922adb9b3b66eca767fa4397ddd18a741ede82c
Rodrigo Azevedo <rod.c.azevedo@gmail.com>
2023-12-09 22:43:02 +0000

    Create custom BaseApi store factory and use it for WalletStore

--- app/javascript/stores/WalletStore.js
+++ app/javascript/stores/WalletStore.js
@@ -1,81 +1,26 @@
-import { defineStore } from 'pinia';
-
 import I18n from '~/utils/I18n.js';
 import { wallets as walletsApi } from '~/api';
 import useNotificationStore from '~/stores/NotificationStore.js';
-import useModalStore from '~/stores/ModalStore.js';
 import { WALLET_FORM_ID } from '~/utils/Constants.js';
+import { defineBaseApiStore } from '~/stores/BaseApiStore.js';
 
-export default defineStore('wallet', {
-  state: () => ({
+export default defineBaseApiStore('wallet', {
+  resourceName: 'wallet',
+  resourcesName: 'wallets',
+  formId: WALLET_FORM_ID,
+  api: walletsApi,
+
+  state: {
     wallets: [],
     idForFormModal: null,
     fetchParams: {
       showDisabled: false,
     },
-  }),
+  },
   getters: {
-    walletForFormModal: (state) => {
-      // Clone the object so it doesn't affect the list before it's actually updated
-      return { ...(state.wallets.find(wallet => wallet.id === state.idForFormModal) ?? {}) };
-    },
     showDisabled: state => state.fetchParams.showDisabled,
   },
   actions: {
-    openFormModal(id) {
-      const modalStore = useModalStore();
-      this.idForFormModal = id;
-      modalStore.show(WALLET_FORM_ID);
-    },
-
-    loadFromProps(wallets) {
-      this.wallets = wallets;
-    },
-
-    setFetchParams(params) {
-      this.fetchParams = Object.assign(this.fetchParams, params);
-    },
-
-    fetch() {
-      walletsApi.index({ query: this.fetchParams }).then(response => this.wallets = response.wallets);
-    },
-
-    create(wallet) {
-      const notificationStore = useNotificationStore();
-      let responseResolve;
-      const returnPromise = new Promise(resolve => responseResolve = resolve);
-
-      walletsApi
-        .create({ data: { wallet } })
-        .then((response) => {
-          this.fetch();
-          notificationStore.notify(response.message, 'success');
-          responseResolve();
-        }).catch((error) => {
-        notificationStore.notify(error.body.message, 'danger');
-      });
-
-      return returnPromise;
-    },
-
-    update(id, wallet) {
-      const notificationStore = useNotificationStore();
-      let responseResolve;
-      const returnPromise = new Promise(resolve => responseResolve = resolve);
-
-      walletsApi
-        .update({ params: { id }, data: { wallet } })
-        .then((response) => {
-          this.fetch();
-          notificationStore.notify(response.message, 'success');
-          responseResolve();
-        }).catch((error) => {
-        notificationStore.notify(error.body.message, 'danger');
-      });
-
-      return returnPromise;
-    },
-
     disable(id) {
       const notificationStore = useNotificationStore();
 
