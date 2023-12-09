271ef1b7df1ea8113c90bdb3d4ecf8da5aeb431a
Rodrigo Azevedo <rod.c.azevedo@gmail.com>
2023-12-09 22:49:57 +0000

    Use new BaseApi factory for TransactionStore

--- app/javascript/stores/TransactionStore.js
+++ app/javascript/stores/TransactionStore.js
@@ -1,33 +1,31 @@
-import { defineStore, storeToRefs } from 'pinia';
+import { defineBaseApiStore } from '~/stores/BaseApiStore';
+import { storeToRefs } from 'pinia';
 import _ from 'lodash';
 
 import usePaginationStore from '~/stores/PaginationStore.js';
-import useModalStore from '~/stores/ModalStore.js';
-import useNotificationStore from '~/stores/NotificationStore.js';
 import { transactions as transactionsApi } from '~/api';
 import { DEBIT_TRANSACTION, CREDIT_TRANSACTION, TRANSACTION_FORM_ID } from '~/utils/Constants.js';
 
-export default defineStore('transaction', {
-  state: () => ({
+export default defineBaseApiStore('transaction', {
+  resourceName: 'transaction',
+  resourcesName: 'transactions',
+  formId: TRANSACTION_FORM_ID,
+  api: transactionsApi,
+
+  state: {
     transactions: [],
-    transactionIdForFormModal: null,
+    idForFormModal: null,
     fetchParams: {
       daysToShow: 30,
       excludeDebits: false,
       excludeCredits: false,
     },
-  }),
+  },
   getters: {
-    daysToShow: (state) => state.fetchParams.daysToShow,
-    excludeDebits: (state) => state.fetchParams.excludeDebits,
-    excludeCredits: (state) => state.fetchParams.excludeCredits,
-    transactionForFormModal: (state) => {
-      // Clone the transaction so it doesn't affect the list before it's actually updated
-      return { ...(state.transactions.find(transaction => transaction.id === state.transactionIdForFormModal) ?? {}) };
-    },
-    groupedTransactions: (state) => {
-      return _.groupBy(state.transactions, 'transactionDate');
-    }
+    daysToShow: state => state.fetchParams.daysToShow,
+    excludeDebits: state => state.fetchParams.excludeDebits,
+    excludeCredits: state => state.fetchParams.excludeCredits,
+    groupedTransactions: state => _.groupBy(state.transactions, 'transactionDate'),
   },
   actions: {
     fetch(options = {}) {
@@ -47,56 +45,13 @@ export default defineStore('transaction', {
         });
     },
 
-    setFetchParams(params) {
-      this.fetchParams = Object.assign(this.fetchParams, params);
-    },
-
-    create(transaction) {
-      let responseResolve;
-      const notificationStore = useNotificationStore();
-      const returnPromise = new Promise(resolve => responseResolve = resolve);
-
-      transactionsApi
-        .create({ data: { transaction } })
-        .then((response) => {
-          notificationStore.notify(response.message, 'success');
-          this.fetch().then(() => responseResolve());
-        }).catch((error) => {
-        notificationStore.notify(error.body.message, 'danger');
-      });
-
-      return returnPromise;
-    },
-
-    update(id, transaction) {
-      const notificationStore = useNotificationStore();
-      let responseResolve;
-      const returnPromise = new Promise(resolve => responseResolve = resolve);
-
-      transactionsApi
-        .update({ params: { id } , data: { transaction } })
-        .then((response) => {
-          notificationStore.notify(response.message, 'success');
-          this.fetch().then(() => responseResolve());
-        }).catch((error) => {
-        notificationStore.notify(error.body.message, 'danger');
-      });
-
-      return returnPromise;
-    },
-
     remove(id) {
       this.transactions = this.transactions.filter(transaction => transaction.id !== id);
     },
+
     setTransactionType(newType) {
       this.fetchParams.excludeCredits = newType === DEBIT_TRANSACTION;
       this.fetchParams.excludeDebits = newType === CREDIT_TRANSACTION;
-    },
-
-    openFormModal(id) {
-      const modalStore = useModalStore();
-      this.transactionIdForFormModal = id;
-      modalStore.show(TRANSACTION_FORM_ID);
     },
   }
 });
