80fc85db4e74f7dbf65c5a8db41bf88cd252c99d
Rodrigo Azevedo <rod.c.azevedo@gmail.com>
2024-01-26 22:11:04 -0300

    Improve Dockerfile and build scripts, integrate some of Rails 7.1 new Dockerfile features

--- deploy/docker/rails_entrypoint.sh
+++ deploy/docker/rails_entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
 
-export RAILS_ENV=production
-export RAILS_SERVE_STATIC_FILES=true
-
-# Run migrations during first run
-bundle exec rails db:migrate
+# Create new or migrate existing database
+bundle exec rails db:prepare
 
 # Remove any pre-existing server.pid.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /finance_micromanager/tmp/pids/server.pid
 
 # Run Dockerfile CMD command
 exec "$@"
