bc78aebb098d1c90929f114cc71bd03282515564
Rodrigo Azevedo <rod.c.azevedo@gmail.com>
2024-01-28 13:05:53 -0300

    Add proper support for manifest.webmanifest file

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'action_cable/engine'
+require_relative '../lib/pwa/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
