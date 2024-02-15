40d44c285b67c7d143f9e5e4683bf44382e1b2af
Rodrigo Azevedo <rod.c.azevedo@gmail.com>
2024-02-15 20:02:12 -0300

    Order currency list when creating new profile

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,6 +2,6 @@
 
 class CurrenciesController < AbstractAuthenticatedController
   def index
-    render json: Money::Currency.all.as_json(only: %w[id iso_code name symbol])
+    render json: Money::Currency.all.sort_by(&:name).as_json(only: %w[id iso_code name symbol])
   end
 end
