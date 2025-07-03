5c38743878589d57a701646b917ff6c13611118f
Rodrigo Azevedo <rod.c.azevedo@gmail.com>
2025-07-03 20:28:05 +0100

    Show see all link for categories and transactions in dashboard

---
+++ app/javascript/components/ui/SeeAllLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <a class="fs-6">
+    {{ t('label') }}
+  </a>
+</template>
+
+<script>
+import I18n from '~/utils/I18n.js';
+
+export default {
+  setup() {
+    const t = I18n.scopedTranslator('views.components.see_all_link');
+
+    return { t };
+  }
+}
+</script>
