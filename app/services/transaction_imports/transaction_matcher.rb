5a092c7e33b2d602adb7c1b94f64f29b8ea3cc93
Rodrigo Azevedo <rod.c.azevedo@gmail.com>
2025-07-30 11:30:32 +0100

    Update Dockerfile to enable jemalloc and YJIT, add Docker build cache to Github action

--- app/services/transaction_imports/transaction_matcher.rb
+++ app/services/transaction_imports/transaction_matcher.rb
@@ -2,8 +2,6 @@
 
 module TransactionImports
   class TransactionMatcher < ApplicationService
-    attr_reader :import_transaction, :transactions, :matches
-
     # Maximum difference in days between two dates so they can be considered to have a half match
     DATE_MATCH_DAYS_THRESHOLD = 7
     # Maximum difference in currency cents units between two amounts so they can be considered to have a half match
@@ -11,57 +9,56 @@ module TransactionImports
     # Minimum match score so that a transaction can be considered to match with this import transaction
     MATCH_SCORE_THRESHOLD     = 1.5
 
-    def initialize(import_transaction, transactions)
-      @import_transaction = import_transaction
-      @transactions       = transactions
-    end
+    class << self
+      def call(import_transaction, transactions)
+        matches   = transactions.each_with_object([]) do |transaction, result|
+          match_data = match_data_for(import_transaction, transaction)
+          result << match_data if match_data
+        end
 
-    def call
-      @matches   = transactions.each_with_object([]) do |transaction, result|
-        match_data = match_data_for(transaction)
-        result << match_data if match_data
+        import_transaction.matches = matches.sort_by { |match| -match[:match_score] }
+        import_transaction
       end
 
-      @import_transaction.matches = matches.sort_by { |match| -match[:match_score] }
-      @import_transaction
-    end
+      private
 
-    private
+      def match_data_for(import_transaction, transaction)
+        match_score      = match_score_for(import_transaction, transaction)
+        return if match_score < MATCH_SCORE_THRESHOLD
 
-    def match_data_for(transaction)
-      match_score      = match_score_for(transaction)
-      return if match_score < MATCH_SCORE_THRESHOLD
+        already_imported = transaction[:import_id].present?
 
-      already_imported = transaction[:import_id].present?
+        { transaction:, match_score:, already_imported: }
+      end
 
-      { transaction:, match_score:, already_imported: }
-    end
+      def match_score_for(import_transaction, transaction)
+        name_match_score(import_transaction, transaction) +
+          date_match_score(import_transaction, transaction) +
+          amount_match_score(import_transaction, transaction)
+      end
 
-    def match_score_for(transaction)
-      name_match_score(transaction) + date_match_score(transaction) + amount_match_score(transaction)
-    end
+      def name_match_score(import_transaction, transaction)
+        return 1 if transaction[:name] == import_transaction.name
 
-    def name_match_score(transaction)
-      return 1 if transaction[:name] == import_transaction.name
+        0
+      end
 
-      0
-    end
+      def date_match_score(import_transaction, transaction)
+        return 0 unless import_transaction.transaction_date
 
-    def date_match_score(transaction)
-      return 0 unless import_transaction.transaction_date
+        date_diff_days = (transaction[:transaction_date] - import_transaction.transaction_date).abs
+        return 1 if date_diff_days.zero?
+        return 0.5 if date_diff_days <= DATE_MATCH_DAYS_THRESHOLD
 
-      date_diff_days = (transaction[:transaction_date] - import_transaction.transaction_date).abs
-      return 1 if date_diff_days.zero?
-      return 0.5 if date_diff_days <= DATE_MATCH_DAYS_THRESHOLD
+        0
+      end
 
-      0
-    end
+      def amount_match_score(import_transaction, transaction)
+        return 1 if transaction[:amount_cents] == import_transaction.amount_cents
+        return 0.5 if (transaction[:amount_cents] - import_transaction.amount_cents).abs < AMOUNT_MATCH_THRESHOLD
 
-    def amount_match_score(transaction)
-      return 1 if transaction[:amount_cents] == import_transaction.amount_cents
-      return 0.5 if (transaction[:amount_cents] - import_transaction.amount_cents).abs < AMOUNT_MATCH_THRESHOLD
-
-      0
+        0
+      end
     end
   end
 end
--- deploy/docker/rails.Dockerfile
+++ deploy/docker/rails.Dockerfile
@@ -45,7 +45,8 @@ FROM ruby:$RUBY_VERSION-slim AS app
 
 # Install runtime dependencies
 RUN apt update -qq &&\
-    apt install -y ca-certificates curl gnupg lsb-release &&\
+    apt upgrade -y --no-install-recommends &&\
+    apt install -y --no-install-recommends ca-certificates curl gnupg lsb-release libjemalloc2 &&\
     sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list' &&\
     curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg &&\
     apt-get update &&\
@@ -53,13 +54,22 @@ RUN apt update -qq &&\
 
 WORKDIR /finance_micromanager
 
+ENV LD_PRELOAD="libjemalloc.so.2" \
+    MALLOC_CONF="background_thread:true,metadata_thp:auto,dirty_decay_ms:5000,muzzy_decay_ms:5000,narenas:2" \
+    RUBY_YJIT_ENABLE="1"
+
 # Copy relevant files from builder stage
 COPY --from=builder --exclude=vendor/cache /finance_micromanager /finance_micromanager
 COPY --from=builder /usr/local/bundle /usr/local/bundle
 
 # Configure entrypoint
 COPY deploy/docker/rails_entrypoint.sh /usr/bin/entrypoint.sh
-RUN chmod +x /usr/bin/entrypoint.sh && date -u '+%d%m%Y%H%M%S' > DEPLOY_TIMESTAMP
+RUN chmod +x /usr/bin/entrypoint.sh && date -u '+%Y%m%d%H%M%S' > DEPLOY_TIMESTAMP
+
+# Precompile bootsnap code for faster boot times
+RUN rm -rf /finance_micromanager/tmp/cache
+RUN bundle exec bootsnap precompile --gemfile app/ lib/
+
 ENTRYPOINT ["entrypoint.sh"]
 EXPOSE 3000
 
